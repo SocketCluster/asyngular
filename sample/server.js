0e695930230115a272fe4c68808dd70c86fdba4d
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-01-03 00:19:54 +0100

    WIP

--- sample/server.js
+++ sample/server.js
@@ -1,33 +1,64 @@
-/*
-  If you plan to run AGC on Kubernetes at some point
-  in the future, avoid changing the environment variable names below as
-  each one has a specific meaning within Asyngular.
-*/
-
-// TODO 2: Use init volume instead of using fsutil.js and dockerwait.js.
-
-const path = require('path');
+const http = require('http');
 const emtase = require('emtase');
-const chokidar = require('chokidar');
+const asyngularServer = require('asyngular-server');
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+const morgan = require('morgan');
+const healthChecker = require('sc-framework-health-check');
 
 const ENVIRONMENT = process.env.ENV || 'dev';
-const ASYNGULAR_WORKER_CONTROLLER = process.env.ASYNGULAR_WORKER_CONTROLLER || path.join(__dirname, 'worker.js');
+const ASYNGULAR_PORT = process.env.ASYNGULAR_PORT || 8000;
+const ASYNGULAR_WS_ENGINE = process.env.ASYNGULAR_WS_ENGINE || 'ws';
+const ASYNGULAR_SOCKET_CHANNEL_LIMIT = Number(process.env.ASYNGULAR_SOCKET_CHANNEL_LIMIT) || 1000;
+const AGC_STATE_SERVER_HOST = process.env.AGC_STATE_SERVER_HOST || null;
+const AGC_STATE_SERVER_PORT = process.env.AGC_STATE_SERVER_PORT || null;
+const AGC_MAPPING_ENGINE = process.env.AGC_MAPPING_ENGINE || null;
+const AGC_CLIENT_POOL_SIZE = process.env.AGC_CLIENT_POOL_SIZE || null;
+const AGC_AUTH_KEY = process.env.AGC_AUTH_KEY || null;
+const AGC_INSTANCE_IP = process.env.AGC_INSTANCE_IP || null;
+const AGC_INSTANCE_IP_FAMILY = process.env.AGC_INSTANCE_IP_FAMILY || null;
+const AGC_STATE_SERVER_CONNECT_TIMEOUT = Number(process.env.AGC_STATE_SERVER_CONNECT_TIMEOUT) || null;
+const AGC_STATE_SERVER_ACK_TIMEOUT = Number(process.env.AGC_STATE_SERVER_ACK_TIMEOUT) || null;
+const AGC_STATE_SERVER_RECONNECT_RANDOMNESS = Number(process.env.AGC_STATE_SERVER_RECONNECT_RANDOMNESS) || null;
 
+let httpServer = emtase(http.createServer());
+let agServer = asyngularServer.attach(httpServer);
+
+let expressApp = express();
 if (ENVIRONMENT === 'dev') {
-  // This will cause the process to reboot when code changes anywhere in the app directory.
-  console.log(`   !! Chokidar is watching for code changes in the ${__dirname} directory`);
-
-  let chokidarOptions = {
-    cwd: __dirname,
-    ignored: ['public', 'node_modules', 'README.md', 'Dockerfile', 'server.js', 'broker.js', /[\/\\]\./, '*.log']
-  };
-  let chokidarWatcher = emtase(chokidar.watch('.', chokidarOptions));
-
-  (async () => {
-    let [filePath] = await chokidarWatcher.listener('change').once();
-    console.log(`   !! File ${filePath} was modified. Restarting worker...`);
-    process.exit(0);
-  })();
+  // Log every HTTP request. See https://github.com/expressjs/morgan for other
+  // available formats.
+  expressApp.use(morgan('dev'));
 }
+expressApp.use(serveStatic(path.resolve(__dirname, 'public')));
 
-require(ASYNGULAR_WORKER_CONTROLLER);
+// Add GET /health-check express route
+healthChecker.attach(agServer, expressApp);
+
+// HTTP request handling loop.
+(async () => {
+  for await (let requestData of httpServer.listener('request')) {
+    expressApp.apply(null, requestData);
+  }
+})();
+
+// Asyngular/WebSocket connection handling loop.
+(async () => {
+  for await (let {socket} of agServer.listener('connection')) {
+    // Handle socket connection.
+  }
+})();
+
+httpServer.listen(ASYNGULAR_PORT);
+
+console.log(
+  `   ${colorText('[Active]', 32)} Asyngular worker with PID ${process.pid} is listening on port ${ASYNGULAR_PORT}`
+);
+
+function colorText(message, color) {
+  if (color) {
+    return `\x1b[${color}m${message}\x1b[0m`;
+  }
+  return message;
+}
